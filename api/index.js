4cac7d3bbe32e9786c0e270d411f3c884013bb32
grantral <grantral@pm.me>
2022-05-29 23:57:37 +0600

    [api] Use cors package for cors settings

--- api/index.js
+++ api/index.js
@@ -1,12 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const gracefulShutdown = require('http-graceful-shutdown');
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 const products = [{
   id: 1,
